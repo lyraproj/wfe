aa8dbd40a7e73ec9a9ddb857168e3a1af3d187cd
Thomas Hallgren <thomas@tada.se>
2019-03-16 15:41:23 +0100

    Ensure that workflow activities that ends with panic are recovered
    This commit ensures that the execution of a workflow activity that
    terminates abnormally is recovered and then rethrown by the workflow's
    Go routine.
    
    Relates to lyraproj/lyra#134

--- api/issues.go
+++ api/issues.go
@@ -6,6 +6,7 @@ const (
 	NoSuchAttribute             = `WF_NO_SUCH_ATTRIBUTE`
 	NoActivityContext           = `WF_NO_ACTIVITY_CONTEXT`
 	MissingRequiredProperty     = `WF_MISSING_REQUIRED_PROPERTY`
+	MultipleErrors              = `WF_MULTIPLE_ERRORS`
 	UnableToLoadRequired        = `WF_UNABLE_TO_LOAD_REQUIRED`
 	UnableToDetermineExternalId = `WF_UNABLE_TO_DETERMINE_EXTERNAL_ID`
 )
@@ -14,6 +15,7 @@ func init() {
 	issue.Hard2(NoSuchAttribute, `%{activity} has no attribute named '%{name}'`, issue.HF{`activity`: issue.Label})
 	issue.Hard(NoActivityContext, `no activity context was found in current scope`)
 	issue.Hard(MissingRequiredProperty, `definition %{service} %{definition} is missing required property '%{key}'`)
+	issue.Hard2(MultipleErrors, `multiple errors: %{errors}`, issue.HF{`errors`: issue.JoinErrors})
 	issue.Hard(UnableToLoadRequired, `unable to load required %{namespace} '%{name}'`)
 	issue.Hard(UnableToDetermineExternalId, `unable to determine external ID for %{style} '%{id}'`)
 }
